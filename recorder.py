127aa02a4faa465689efa1c6d276762679dcbc84
creminn <erman165243@gmail.com>
2025-03-01 00:42:37 +0300

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -27,3 +27,6 @@ def record(record_active, frames):
     sound_file.setframerate(44100)
     sound_file.writeframes(b''.join(frames))
     sound_file.close()vvcb vczb
+
+
+    dfgl;'mksdf'pgklm,s'pdflogsdf
